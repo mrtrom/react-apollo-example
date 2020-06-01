8c3800ef6ca77e6d730478c144bc1a93bf6d96d7
Alexander Ochoa <ochoaseguralex@gmail.com>
2020-06-01 02:39:12 -0500

    Added websocket connection for Subscriptions

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export default {
-  apiUri: process.env.REACT_APP_API_URI || 'http://localhost:8080/graphql',
+  apiHttpUri: process.env.REACT_APP_API_HTTP_URI || 'http://localhost:8080/graphql',
+  apiWsUri: process.env.REACT_APP_API_WS_URI || 'ws://localhost:8080/graphql',
   auth0: {
     domain: process.env.REACT_APP_AUTH0_DOMAIN || '',
     clientID: process.env.REACT_APP_AUTH0_CLIENT_ID || '',
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import { render } from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { WebSocketLink } from 'apollo-link-ws';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Pages from './pages';
 import config from './config';
+
+const wsLink = new WebSocketLink({
+  uri: config.apiWsUri,
+  options: {
+    reconnect: true,
+  },
+});
+
+const httpLink = new HttpLink({
+  uri: config.apiHttpUri,
+  headers: {
+    // authorization: localStorage.getItem('token'),
+    'client-name': 'react-apollo-example [web]',
+    'client-version': '1.0.0',
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  wsLink,
+  httpLink
+);
 
 const cache = new InMemoryCache();
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
-  link: new HttpLink({
-    uri: config.apiUri,
-    headers: {
-      // authorization: localStorage.getItem('token'),
-      'client-name': 'react-apollo-example [web]',
-      'client-version': '1.0.0',
-    },
-  }),
+  link,
 });
 
 const initData = {
